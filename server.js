1ba384e542ff32273f72e6f34a012105898a360a
Sam Rudd <sam.rudd@accesso.com>
2020-12-02 12:39:35 -0800

    use json

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT;
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 var database = mongoose.createConnection(db.url);
 
